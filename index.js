d8b4a1b80b47469cae5d8a12a09f2a10d66ff58b
George Lugo <stg77713@gmail.com>
2020-03-14 03:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
-function nestedTarget(){
-  return document.querySelector('#nested.target');
+function nestedTarget() {
+  return document.querySelector('#nested .target');
 }
 
 function increaseRankBy(n){
