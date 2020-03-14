3d17b1de11a7294f081a8d2d508e052b18dc6330
George Lugo <stg77713@gmail.com>
2020-03-14 03:57:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+  
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  
+  return node;
+}
 
